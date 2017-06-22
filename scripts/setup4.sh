c254da42829c4f642ac1f608b668d281438c4b30
helgadeville <void@buntownik.pl>
2017-06-22 23:29:55 +0700

    Some changes

--- scripts/setup4.sh
+++ scripts/setup4.sh
@@ -5,5 +5,8 @@ opkg update
 opkg install lighttpd
 opkg install lighttpd-mod-access lighttpd-mod-alias lighttpd-mod-auth lighttpd-mod-authn_file lighttpd-mod-cgi lighttpd-mod-evasive
 echo Now preparing GUI
+cp /root/etc/lighttpd/.htdigest /etc/lighttpd
 echo TODO
+# restart server
+/etc/init.d/lighttpd restart
 
