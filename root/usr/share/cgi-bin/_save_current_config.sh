e28c60ba12f5d4063cb8d06b489af57a05c6c6d7
Helga de Ville <szklarska@hpe.com>
2017-07-21 22:28:46 +0200

    More fixes

--- root/usr/share/cgi-bin/_save_current_config.sh
+++ root/usr/share/cgi-bin/_save_current_config.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-files1=`find /etc/config/* -type f`
-files2=`find /etc/lighttpd/* -type f | grep -v ".htaccess"`
-files3=`find /etc/openvpn/* -type f | grep -v '\(/etc/openvpn/upvpn\|/etc/openvpn/downvpn\|/etc/openvpn/startingvpn\|/etc/openvpn/stoppingvpn\|/etc/openvpn/errorvpn\)'`
-files4="/etc/bind/named.conf /etc/dnsmasq.conf"
-files="$files1 $files2 $files3 $files4"
-tar czf "/root/configurations/custom/$file.cgz" "$files" > /dev/null 2>&1
+find /etc/config/* -type f > /tmp/_current.list
+find /etc/lighttpd/* -type f | grep -v ".htaccess" >> /tmp/_current.list
+find /etc/openvpn/* -type f | grep -v '\(/etc/openvpn/upvpn\|/etc/openvpn/downvpn\|/etc/openvpn/startingvpn\|/etc/openvpn/stoppingvpn\|/etc/openvpn/errorvpn\)' >> /tmp/_current.list
+echo "/etc/bind/named.conf" >> /tmp/_current.list
+echo "/etc/dnsmasq.conf" >> /tmp/_current.list
+tar czf "/root/configurations/custom/$file.cgz" -T /tmp/_current.list > /dev/null 2>&1
+rm /tmp/_current.list
