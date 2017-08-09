e92f658af8917022df84ff8a8f26be871e90db09
Helga de Ville <szklarska@hpe.com>
2017-08-09 12:27:10 +0200

    Fix

--- root/usr/share/cgi-bin/_restart.sh
+++ root/usr/share/cgi-bin/_restart.sh
@@ -2,10 +2,11 @@
 sleep 1
 /etc/init.d/network restart > /dev/null 2>&1
 /etc/init.d/netwait start > /dev/null 2>&1
+/etc/init.d/firewall reload > /dev/null 2>&1
 /etc/init.d/dnsmasq reload > /dev/null 2>&1
 /etc/init.d/named restart > /dev/null 2>&1
 if [ -n "$lan" ];
  then
   /etc/init.d/lighttpd reload > /dev/null 2>&1
-  /etc/init.d/dropbear restart > /dev/null 2>&1
+  /etc/init.d/dropbear reload > /dev/null 2>&1
 fi
