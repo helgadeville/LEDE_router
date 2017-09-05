3f35fc3a738ae783e2f1e71309305273ae40095e
Helga de Ville <szklarska@hpe.com>
2017-09-05 15:41:41 +0200

    Fix

--- root/usr/share/cgi-bin/_get_vpn_config.sh
+++ root/usr/share/cgi-bin/_get_vpn_config.sh
@@ -4,7 +4,7 @@ if [ -z "$file" ];
   user=`head -n 1 /etc/openvpn/.auth.txt 2>/dev/null`
   [ -n "$user" ] && echo "#user=$user"
   pass=`head -n 2 /etc/openvpn/.auth.txt 2>/dev/null`
-  # [ -n "$pass" ] && echo "#pass=$pass"
+  [ -n "$pass" ] && echo "#pass=$pass"
   cat /etc/openvpn/vpn.conf 2> /dev/null
  else
   cat "/etc/openvpn/configurations/$file" 2> /dev/null
