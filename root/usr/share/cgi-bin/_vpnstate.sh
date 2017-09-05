8ed4c6ce1af9199dc3bde09e612630c43914033a
Helga de Ville <szklarska@hpe.com>
2017-09-05 21:02:37 +0200

    More fixes

--- root/usr/share/cgi-bin/_vpnstate.sh
+++ root/usr/share/cgi-bin/_vpnstate.sh
@@ -1,24 +1,18 @@
 #!/bin/sh
 VTUN=`ifconfig | grep tun`
 [ -n "$VTUN" ] && VTUN=yes
-OVPN=`pgrep openvpn`
+OVPN=`ps | grep open[v]pn`
 VSTART=`ps | grep s[t]artingvpn`
 VSTOP=`ps | grep s[t]oppingvpn`
 VERR=`ps | grep e[r]rorvpn`
-if [ -n "$VSTART" -o -n "$VSTOP" -o -n "$VERR" ];
+if [ -n "$OVPN" -a -n "$VTUN" ];
  then
-  [ -n "$VSTART" ] && VPN=starting
-  [ -n "$VSTOP" ] && VPN=stopping
-  [ -n "$VSTART" -a -n "$VSTOP" ] && VPN=error
-  [ -n "$VERR" ] && VPN=error
+  VPN=up
  else
-  if [ -n "$OVPN" -a -n "$VTUN" ];
-   then
-    VPN=up
-   else
-    VPN=down
-    [ -n "$OVPN" -a -z "$VTUN" ] && VPN=stopping
-    [ -z "$OVPN" -a -n "$VTUN" ] && VPN=error
-  fi
+  VPN=down
 fi
+[ -n "$VSTART" ] && VPN=starting
+[ -n "$VSTOP" ] && VPN=stopping
+[ -n "$VSTART" -a -n "$VSTOP" ] && VPN=error
+[ -n "$VERR" ] && VPN=error
 echo $VPN
--- scripts/setup1.sh
+++ scripts/setup1.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-echo Change LAN address to 10.10.10.10
-# change lan address to 10.10.10.10
-uci set network.lan.ipaddr=10.10.10.10
-# remove wan6 configuration
-uci del network.wan6
-uci commit
-# reload network
-echo *** YOU NEED TO RECONNECT NOW ***
-/etc/init.d/network reload
-
+echo TO PROCEED, YOU NEED TO HAVE EXT4 USB PENDRIVE IN USB PORT
+echo ALSO YOU NEED ROOT.TGZ AND EXPORT.TGZ ON THE PENDRIVE
+mount /dev/sda1 /mnt
+cd /mnt
+tar xzf root.tgz
+cp /mnt/root/etc/config/fstab /etc/config
+chmod 444 /etc/config/fstab
+service fstab enable
+readlink -f /etc/rc.d/*fstab
+tar -C /overlay/ -c . -f - | tar -C /mnt/ -xf -
+sync && cd / && umount /dev/sda1
+# NEED TO REBOOT
+echo REBOOT NOW ! AFTER REBOOT MAKE SURE TO HAVE INTERNET CONNECTION !!!
