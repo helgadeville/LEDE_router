1a43ddf3593760f0378c0d68556440c48a89c60a
Helga de Ville <szklarska@hpe.com>
2017-09-08 19:33:54 +0200

    Introduced upgrades

---
+++ root/usr/share/cgi-bin/_upgrade.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+# accepted parameters: $data
+# check file
+if [ -z "$data"];
+ then
+  exit 1
+fi
+# prepare tmp directory
+TMP="/tmp/_upgrade"
+rm -rf "$TMP"
+mkdir "$TMP"
+echo "$data" | base64 --decode | openssl enc -d -aes-256-cbc -pass file:/root/password > "$TMP/_image.tgz"
+cd "$TMP"
+# unzip and copy all
+skip=no
+tar xzf _image.tgz 2> /dev/null
+if [ $? -gt 0 ];
+ then
+  skip=yes
+fi
+rm _image.tgz
+if [ "$skip" = "no" ];
+ then
+  # old files removal
+  if [ -f "-" ];
+   then
+    while read -r line; 
+     do 
+      rm -rf $line ; 
+     done < -
+  fi
+
+  # new files
+  if [ -d "+" ];
+   then
+    cd +
+    chown -R root:root *
+    [ -f usr/bin/sudo ] && chmod 4755 usr/bin/sudo
+    cp -R * /
+    cd ..
+  fi
+
+  # WWW interface
+  if [ -f export.tgz ];
+   then
+    tar xzf export.tgz
+    rm -rf /www/*
+    mv export/* /www/
+    rmdir export
+    mv export.tgz /root
+  fi
+  # VERSION setup
+  [ -f version ] && mv version /www/
+fi
+
+cd /
+rm -rf "$TMP"
+[ "$skip" = "yes" ] && exit 1
