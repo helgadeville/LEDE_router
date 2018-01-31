4c460832fead71618ffaf31fd50190eec63dbf3f
Helga de Ville <szklarska@dxc.com>
2018-01-31 18:37:08 +0100

    Small fix

--- root/usr/share/cgi-bin/_radios.sh
+++ root/usr/share/cgi-bin/_radios.sh
@@ -3,7 +3,8 @@ RADIOS=`uci show wireless | grep "wifi-device" | sed 's/wireless\.// ; s/=.*//'`
 WIFI="["
 for radio in $RADIOS
  do
-  STATE=`uci show wireless.$radio.disabled | sed "s/.*disabled=// ; s/\'//g ; s/0$/enabled/ ; s/1$/disabled/"` ;
+  STATE=`uci show wireless.$radio.disabled 2> /dev/null | sed "s/.*disabled=// ; s/\'//g ; s/0$/enabled/ ; s/1$/disabled/"` ;
+  [ -z "$STATE" ] && STATE=enabled
   WIFI="$WIFI{\"#\":\"$radio\",\"state\":\"$STATE\"}," ;
 done
 WIFI=`echo $WIFI | sed 's/,$/]/'`
