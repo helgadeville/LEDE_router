823af6a7c93a7d9110f857c54056ca943533b79d
Helga de Ville <szklarska@dxc.com>
2018-02-11 23:50:23 +0100

    Minor fixes

--- root/usr/share/cgi-bin/_set_dns.sh
+++ root/usr/share/cgi-bin/_set_dns.sh
@@ -10,4 +10,5 @@ uci del network.wan.dns > /dev/null 2>&1
 [ -n "$dns1" ] && uci add_list network.wan.dns="$dns1"
 [ -n "$dns2" ] && uci add_list network.wan.dns="$dns2"
 uci commit
-
+# restart network
+sudo -b /usr/share/cgi-bin/_restart.sh
